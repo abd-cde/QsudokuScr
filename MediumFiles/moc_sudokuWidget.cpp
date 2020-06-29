615e7c9849a4c774ddfa10676c390cb9175df1d9
waitfo <2049332830@qq.com>
2020-06-29 12:17:43 +0800

    Initial commit

---
+++ MediumFiles/moc_sudokuWidget.cpp
@@ -0,0 +1,313 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'sudokuWidget.h'
+**
+** Created: Mon May 19 14:53:06 2014
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../MainWidget/sudokuWidget.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'sudokuWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.6.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_sudokuMain[] = {
+
+ // content:
+       4,       // revision
+       0,       // classname
+       0,    0, // classinfo
+     105,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      12,   11,   11,   11, 0x08,
+      26,   11,   11,   11, 0x08,
+      43,   11,   11,   11, 0x08,
+      58,   11,   11,   11, 0x08,
+      70,   11,   11,   11, 0x08,
+      81,   11,   11,   11, 0x08,
+      91,   11,   11,   11, 0x08,
+     104,   11,   11,   11, 0x08,
+     114,   11,   11,   11, 0x08,
+     128,  123,   11,   11, 0x08,
+     149,  123,   11,   11, 0x08,
+     175,   11,   11,   11, 0x08,
+     186,   11,   11,   11, 0x08,
+     198,   11,   11,   11, 0x08,
+     224,   11,   11,   11, 0x08,
+     230,   11,   11,   11, 0x08,
+     238,   11,   11,   11, 0x08,
+     246,   11,   11,   11, 0x08,
+     254,   11,   11,   11, 0x08,
+     262,   11,   11,   11, 0x08,
+     270,   11,   11,   11, 0x08,
+     278,   11,   11,   11, 0x08,
+     286,   11,   11,   11, 0x08,
+     294,   11,   11,   11, 0x08,
+     302,   11,   11,   11, 0x08,
+     310,   11,   11,   11, 0x08,
+     318,   11,   11,   11, 0x08,
+     326,   11,   11,   11, 0x08,
+     334,   11,   11,   11, 0x08,
+     342,   11,   11,   11, 0x08,
+     350,   11,   11,   11, 0x08,
+     358,   11,   11,   11, 0x08,
+     366,   11,   11,   11, 0x08,
+     374,   11,   11,   11, 0x08,
+     382,   11,   11,   11, 0x08,
+     390,   11,   11,   11, 0x08,
+     398,   11,   11,   11, 0x08,
+     406,   11,   11,   11, 0x08,
+     414,   11,   11,   11, 0x08,
+     422,   11,   11,   11, 0x08,
+     430,   11,   11,   11, 0x08,
+     438,   11,   11,   11, 0x08,
+     446,   11,   11,   11, 0x08,
+     454,   11,   11,   11, 0x08,
+     462,   11,   11,   11, 0x08,
+     470,   11,   11,   11, 0x08,
+     478,   11,   11,   11, 0x08,
+     486,   11,   11,   11, 0x08,
+     494,   11,   11,   11, 0x08,
+     502,   11,   11,   11, 0x08,
+     510,   11,   11,   11, 0x08,
+     518,   11,   11,   11, 0x08,
+     526,   11,   11,   11, 0x08,
+     534,   11,   11,   11, 0x08,
+     542,   11,   11,   11, 0x08,
+     550,   11,   11,   11, 0x08,
+     558,   11,   11,   11, 0x08,
+     566,   11,   11,   11, 0x08,
+     574,   11,   11,   11, 0x08,
+     582,   11,   11,   11, 0x08,
+     590,   11,   11,   11, 0x08,
+     598,   11,   11,   11, 0x08,
+     606,   11,   11,   11, 0x08,
+     614,   11,   11,   11, 0x08,
+     622,   11,   11,   11, 0x08,
+     630,   11,   11,   11, 0x08,
+     638,   11,   11,   11, 0x08,
+     646,   11,   11,   11, 0x08,
+     654,   11,   11,   11, 0x08,
+     662,   11,   11,   11, 0x08,
+     670,   11,   11,   11, 0x08,
+     678,   11,   11,   11, 0x08,
+     686,   11,   11,   11, 0x08,
+     694,   11,   11,   11, 0x08,
+     702,   11,   11,   11, 0x08,
+     710,   11,   11,   11, 0x08,
+     718,   11,   11,   11, 0x08,
+     726,   11,   11,   11, 0x08,
+     734,   11,   11,   11, 0x08,
+     742,   11,   11,   11, 0x08,
+     750,   11,   11,   11, 0x08,
+     758,   11,   11,   11, 0x08,
+     766,   11,   11,   11, 0x08,
+     774,   11,   11,   11, 0x08,
+     782,   11,   11,   11, 0x08,
+     790,   11,   11,   11, 0x08,
+     798,   11,   11,   11, 0x08,
+     806,   11,   11,   11, 0x08,
+     814,   11,   11,   11, 0x08,
+     822,   11,   11,   11, 0x08,
+     830,   11,   11,   11, 0x08,
+     838,   11,   11,   11, 0x08,
+     846,   11,   11,   11, 0x08,
+     854,   11,   11,   11, 0x08,
+     862,   11,   11,   11, 0x08,
+     870,   11,   11,   11, 0x08,
+     878,   11,   11,   11, 0x08,
+     885,   11,   11,   11, 0x08,
+     892,   11,   11,   11, 0x08,
+     899,   11,   11,   11, 0x08,
+     906,   11,   11,   11, 0x08,
+     913,   11,   11,   11, 0x08,
+     920,   11,   11,   11, 0x08,
+     927,   11,   11,   11, 0x08,
+     934,   11,   11,   11, 0x08,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_sudokuMain[] = {
+    "sudokuMain\0\0mainTimerUp()\0connectTimerUp()\0"
+    "clickTimerUp()\0startGame()\0setTable()\0"
+    "fileSet()\0resetTable()\0autoSet()\0"
+    "giveUp()\0text\0changeTheme(QString)\0"
+    "changeBackground(QString)\0enableIP()\0"
+    "expanatry()\0processPendingDatagrams()\0"
+    "win()\0pBk11()\0pBk12()\0pBk13()\0pBk14()\0"
+    "pBk15()\0pBk16()\0pBk17()\0pBk18()\0pBk19()\0"
+    "pBk21()\0pBk22()\0pBk23()\0pBk24()\0pBk25()\0"
+    "pBk26()\0pBk27()\0pBk28()\0pBk29()\0pBk31()\0"
+    "pBk32()\0pBk33()\0pBk34()\0pBk35()\0pBk36()\0"
+    "pBk37()\0pBk38()\0pBk39()\0pBk41()\0pBk42()\0"
+    "pBk43()\0pBk44()\0pBk45()\0pBk46()\0pBk47()\0"
+    "pBk48()\0pBk49()\0pBk51()\0pBk52()\0pBk53()\0"
+    "pBk54()\0pBk55()\0pBk56()\0pBk57()\0pBk58()\0"
+    "pBk59()\0pBk61()\0pBk62()\0pBk63()\0pBk64()\0"
+    "pBk65()\0pBk66()\0pBk67()\0pBk68()\0pBk69()\0"
+    "pBk71()\0pBk72()\0pBk73()\0pBk74()\0pBk75()\0"
+    "pBk76()\0pBk77()\0pBk78()\0pBk79()\0pBk81()\0"
+    "pBk82()\0pBk83()\0pBk84()\0pBk85()\0pBk86()\0"
+    "pBk87()\0pBk88()\0pBk89()\0pBk91()\0pBk92()\0"
+    "pBk93()\0pBk94()\0pBk95()\0pBk96()\0pBk97()\0"
+    "pBk98()\0pBk99()\0pBk1()\0pBk2()\0pBk3()\0"
+    "pBk4()\0pBk5()\0pBk6()\0pBk7()\0pBk8()\0"
+    "pBk9()\0"
+};
+
+const QMetaObject sudokuMain::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_sudokuMain,
+      qt_meta_data_sudokuMain, 0 }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &sudokuMain::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *sudokuMain::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *sudokuMain::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_sudokuMain))
+        return static_cast<void*>(const_cast< sudokuMain*>(this));
+    if (!strcmp(_clname, "Ui_MainForm"))
+        return static_cast< Ui_MainForm*>(const_cast< sudokuMain*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int sudokuMain::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: mainTimerUp(); break;
+        case 1: connectTimerUp(); break;
+        case 2: clickTimerUp(); break;
+        case 3: startGame(); break;
+        case 4: setTable(); break;
+        case 5: fileSet(); break;
+        case 6: resetTable(); break;
+        case 7: autoSet(); break;
+        case 8: giveUp(); break;
+        case 9: changeTheme((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 10: changeBackground((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 11: enableIP(); break;
+        case 12: expanatry(); break;
+        case 13: processPendingDatagrams(); break;
+        case 14: win(); break;
+        case 15: pBk11(); break;
+        case 16: pBk12(); break;
+        case 17: pBk13(); break;
+        case 18: pBk14(); break;
+        case 19: pBk15(); break;
+        case 20: pBk16(); break;
+        case 21: pBk17(); break;
+        case 22: pBk18(); break;
+        case 23: pBk19(); break;
+        case 24: pBk21(); break;
+        case 25: pBk22(); break;
+        case 26: pBk23(); break;
+        case 27: pBk24(); break;
+        case 28: pBk25(); break;
+        case 29: pBk26(); break;
+        case 30: pBk27(); break;
+        case 31: pBk28(); break;
+        case 32: pBk29(); break;
+        case 33: pBk31(); break;
+        case 34: pBk32(); break;
+        case 35: pBk33(); break;
+        case 36: pBk34(); break;
+        case 37: pBk35(); break;
+        case 38: pBk36(); break;
+        case 39: pBk37(); break;
+        case 40: pBk38(); break;
+        case 41: pBk39(); break;
+        case 42: pBk41(); break;
+        case 43: pBk42(); break;
+        case 44: pBk43(); break;
+        case 45: pBk44(); break;
+        case 46: pBk45(); break;
+        case 47: pBk46(); break;
+        case 48: pBk47(); break;
+        case 49: pBk48(); break;
+        case 50: pBk49(); break;
+        case 51: pBk51(); break;
+        case 52: pBk52(); break;
+        case 53: pBk53(); break;
+        case 54: pBk54(); break;
+        case 55: pBk55(); break;
+        case 56: pBk56(); break;
+        case 57: pBk57(); break;
+        case 58: pBk58(); break;
+        case 59: pBk59(); break;
+        case 60: pBk61(); break;
+        case 61: pBk62(); break;
+        case 62: pBk63(); break;
+        case 63: pBk64(); break;
+        case 64: pBk65(); break;
+        case 65: pBk66(); break;
+        case 66: pBk67(); break;
+        case 67: pBk68(); break;
+        case 68: pBk69(); break;
+        case 69: pBk71(); break;
+        case 70: pBk72(); break;
+        case 71: pBk73(); break;
+        case 72: pBk74(); break;
+        case 73: pBk75(); break;
+        case 74: pBk76(); break;
+        case 75: pBk77(); break;
+        case 76: pBk78(); break;
+        case 77: pBk79(); break;
+        case 78: pBk81(); break;
+        case 79: pBk82(); break;
+        case 80: pBk83(); break;
+        case 81: pBk84(); break;
+        case 82: pBk85(); break;
+        case 83: pBk86(); break;
+        case 84: pBk87(); break;
+        case 85: pBk88(); break;
+        case 86: pBk89(); break;
+        case 87: pBk91(); break;
+        case 88: pBk92(); break;
+        case 89: pBk93(); break;
+        case 90: pBk94(); break;
+        case 91: pBk95(); break;
+        case 92: pBk96(); break;
+        case 93: pBk97(); break;
+        case 94: pBk98(); break;
+        case 95: pBk99(); break;
+        case 96: pBk1(); break;
+        case 97: pBk2(); break;
+        case 98: pBk3(); break;
+        case 99: pBk4(); break;
+        case 100: pBk5(); break;
+        case 101: pBk6(); break;
+        case 102: pBk7(); break;
+        case 103: pBk8(); break;
+        case 104: pBk9(); break;
+        default: ;
+        }
+        _id -= 105;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
